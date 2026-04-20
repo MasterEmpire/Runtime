99e3fa141c052fa6129f8327fa51667cc26793a2
GetyeTek <getyeteklu2@gmail.com>
2026-04-20 18:37:01 +0300

    Fix compilation errors: Move Interceptor to Bridge and fix V

--- app/src/main/java/com/speedster/SpeedsterAccessibilityService.kt
+++ app/src/main/java/com/speedster/SpeedsterAccessibilityService.kt
@@ -6,15 +6,11 @@ import android.accessibilityservice.AccessibilityService
 import android.view.KeyEvent
 
 class SpeedsterAccessibilityService : AccessibilityService() {
-    companion object {
-        var keyInterceptor: ((KeyEvent) -> Boolean)? = null
-    }
-
     override fun onAccessibilityEvent(event: android.view.accessibility.AccessibilityEvent?) {}
     override fun onInterrupt() {}
 
     override fun onKeyEvent(event: KeyEvent): Boolean {
-        return keyInterceptor?.invoke(event) ?: super.onKeyEvent(event)
+        return DynamicEntry.keyInterceptor?.invoke(event) ?: super.onKeyEvent(event)
     }
 
     override fun onServiceConnected() {
