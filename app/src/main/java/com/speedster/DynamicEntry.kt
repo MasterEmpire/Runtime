cd236f9afe376915183b5a4ff6cb4c681a2166df
GetyeTek <getyeteklu2@gmail.com>
2026-04-20 17:52:24 +0300

    Implement ZIP-based Bundle system for dynamic resources

--- app/src/main/java/com/speedster/DynamicEntry.kt
+++ app/src/main/java/com/speedster/DynamicEntry.kt
@@ -7,7 +7,9 @@ import androidx.compose.runtime.Composable
  * The Secret Handshake.
  * Your dynamic payload MUST implement this as 'com.speedster.payload.PayloadEntry'
  */
+import java.io.File
+
 interface DynamicEntry {
     @Composable
-    fun Render(context: Context)
+    fun Render(context: Context, resDir: File)
 }
