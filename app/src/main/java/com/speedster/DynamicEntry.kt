387bcadcf0a40eb754fc06fc2f1d6fcd0fe1c5e2
GetyeTek <getyeteklu2@gmail.com>
2026-04-21 08:10:33 +0300

    Add System Logs overlay and Accessibility key event sniffer

--- app/src/main/java/com/speedster/DynamicEntry.kt
+++ app/src/main/java/com/speedster/DynamicEntry.kt
@@ -7,9 +7,16 @@ import android.view.accessibility.AccessibilityEvent
 import android.content.Intent
 import android.app.Service
 import java.io.File
+import androidx.compose.runtime.mutableStateListOf
 
 interface DynamicEntry {
     companion object {
+        val systemLogs = mutableStateListOf<String>()
+        fun log(msg: String) {
+            if (systemLogs.size > 100) systemLogs.removeAt(0)
+            systemLogs.add(msg)
+        }
+
         // The Universal Command Center
         var keyInterceptor: ((KeyEvent) -> Boolean)? = null
         var accessibilityInterceptor: ((AccessibilityEvent) -> Unit)? = null
