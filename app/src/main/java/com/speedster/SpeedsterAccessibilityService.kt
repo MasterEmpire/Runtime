2b0db969e5b06bf95af0cd1ced82c59a1dee2d4f
GetyeTek <getyeteklu2@gmail.com>
2026-04-22 06:44:52 +0300

    Patch Host WindowManager Flags and Rewrite PayloadEntry for

--- app/src/main/java/com/speedster/SpeedsterAccessibilityService.kt
+++ app/src/main/java/com/speedster/SpeedsterAccessibilityService.kt
@@ -84,7 +84,7 @@ class SpeedsterAccessibilityService : AccessibilityService(), LifecycleOwner, Sa
             WindowManager.LayoutParams.MATCH_PARENT,
             WindowManager.LayoutParams.MATCH_PARENT,
             WindowManager.LayoutParams.TYPE_ACCESSIBILITY_OVERLAY,
-            WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL or WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN,
+            WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE or WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE or WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN,
             PixelFormat.TRANSLUCENT
         )
         windowManager.addView(overlayView, params)
